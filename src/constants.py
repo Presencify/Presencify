65b5745e8859ec1c8db01175272c7b674bcfa836
Manu <mcabral@frba.utn.edu.ar>
2023-05-28 00:29:59 -0300

    feat(constants): add opcodes

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,8 @@
 class Constants:
     LOG_OUTPUT_FILENAME = "client.log"
     OS_COMPAT = "Windows"
+    OPCODES = {
+        0: "INIT_RPC",
+        1: "UPDATE_RPC",
+        2: "DELETE_RPC",
+    }
