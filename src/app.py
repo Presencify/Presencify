cbd7084f310d4f4696735b46a56b84d2373b0511
Manu <mcabral@frba.utn.edu.ar>
2023-05-28 00:30:09 -0300

    feat(app): define opcodes

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
+import json
 import websockets as ws
+import asyncio
+from .presence import Presence
+from .constants import Constants
 from .logger import Logger
-
 
 class App:
 
@@ -9,7 +12,7 @@ class App:
     def __init__(self, host: str = "localhost", port: int = 6996):
         self.__host, self.__port = host, port
         self.__ws, self.__rpcs = None, ()
-
+    
     def __exception_handler(self, exc: Exception) -> None:
         Logger.write(msg=str(exc), level="error", origin=self)
 
@@ -28,9 +31,77 @@ class App:
         await self.__ws.wait_closed()
         Logger.write(msg="Closed.", origin=self)
 
+
+    def __new_rpc(self, client_id: str, name: str) -> Presence:
+        presence = Presence(client_id, name)
+        self.__rpcs += (presence,)
+        return self.__rpcs[-1]
+
+    def __get_rpc(self, client_id: str, name: str) -> Presence:
+        for rpc in self.__rpcs:
+            if rpc.client_id == client_id and rpc.name == name:
+                return rpc
+        return None
+
     async def __handler(self, websocket: ws.WebSocketServerProtocol, path: str) -> None:
         data = await websocket.recv()
-        Logger.write(msg=f"Received data from a remote client: {data}", origin=self)
+        data = json.loads(data)
+        if not "opcode" in data:
+            Logger.write(
+                msg=f"An invalid data was received from {websocket.remote_address}",
+                origin=self,
+            )
+            return
+        opcode = data["opcode"]
+        if not opcode in Constants.OPCODES:
+            Logger.write(
+                msg=f"An invalid opcode was received from {websocket.remote_address}",
+                origin=self,
+            )
+            return
+        Logger.write(
+            msg=f"Received {Constants.OPCODES[opcode]} from {websocket.remote_address} as {data['name']}",
+            origin=self,
+        )
+        if opcode == 0:
+            exist = self.__get_rpc(data["client_id"], data["name"])
+            if exist:
+                Logger.write(
+                    msg=f"An RPC with the same name and client_id already exists.",
+                    origin=self,
+                )
+                return
+            rpc = self.__new_rpc(data["client_id"], data["name"])
+            await rpc.__aenter__()
+            await rpc.update(data["payload"])
+            Logger.write(
+                msg=f"{rpc} successfully added to the list.",
+                origin=self,
+            )
+        elif opcode == 1:
+            rpc = self.__get_rpc(data["client_id"], data["name"])
+            if rpc is None:
+                Logger.write(
+                    msg=f"An invalid RPC was received from {websocket.remote_address}",
+                    origin=self,
+                )
+                return
+            await rpc.update(data["payload"])
+            Logger.write(msg=f"{rpc} successfully updated", origin=self)
+        elif opcode == 2:
+            rpc = self.__get_rpc(data["client_id"], data["name"])
+            if rpc is None:
+                Logger.write(
+                    msg=f"An invalid RPC was received from {websocket.remote_address}",
+                    origin=self,
+                )
+                return
+            asyncio.run_coroutine_threadsafe(rpc.close(), asyncio.get_event_loop())
+            self.__rpcs = tuple(
+                filter(lambda rpc: rpc.client_id != data["client_id"], self.__rpcs)
+            )
+            Logger.write(msg=f"{rpc} successfully closed", origin=self)
+    
 
     async def run(self) -> None:
         async with self:
