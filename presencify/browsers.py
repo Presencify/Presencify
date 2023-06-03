9eb35220930bad38210b13081f463b2d1275f263
Manu <mcabral@frba.utn.edu.ar>
2023-06-02 23:34:46 -0300

    feat: add chrome browsers support

---
+++ presencify/browsers.py
@@ -0,0 +1,58 @@
+BROWSERS = [
+    {
+        "name": "Chrome",
+        "fullname": "Google Chrome",
+        "process": {"win32": "chrome.exe", "linux": "google-chrome"},
+        "progid": "ChromeHTML",
+        "chromium": True,
+    },
+    {
+        "name": "Yandex",
+        "fullname": "Yandex Browser",
+        "process": {"win32": "browser.exe", "linux": "yandex-browser"},
+        "chromium": True,
+        "progid": "YandexHTML",
+    },
+    {
+        "name": "Edge",
+        "fullname": "Microsoft Edge",
+        "process": {"win32": "msedge.exe", "linux": "sure??"},
+        "progid": "EdgeHTML",
+        "chromium": True,
+    },
+    {
+        "name": "Opera",
+        "fullname": "Opera",
+        "process": {"win32": "opera.exe", "linux": "..."},
+        "progid": "OperaHTML",
+        "chromium": True,
+    },
+    {
+        "name": "Opera GXStable",
+        "fullname": "Opera GX",
+        "process": {"win32": "opera.exe", "linux": "..."},
+        "progid": "OperaGXHTML",
+        "chromium": True,
+    },
+    {
+        "name": "OperaDev",
+        "fullname": "Opera Developer (Opera One)",
+        "process": {"win32": "opera.exe", "linux": "..."},
+        "progid": "Operadeveloper",
+        "chromium": True,
+    },
+    {
+        "name": "Vivaldi",
+        "fullname": "Vivaldi Browser",
+        "process": {"win32": "vivaldi.exe", "linux": "..."},
+        "progid": "VivaldiHTML",
+        "chromium": True,
+    },
+    {
+        "name": "Brave",
+        "fullname": "Brave Browser",
+        "process": {"win32": "brave.exe", "linux": "..."},
+        "progid": "BraveHTML",
+        "chromium": True,
+    },
+]
