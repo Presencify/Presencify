61e74ae0792dd8ea97ced4c00af817b16902a8a1
Manu <mcabral@frba.utn.edu.ar>
2023-06-04 21:36:55 -0300

    feat(__init__): include utils and constants

--- presencify/__init__.py
+++ presencify/__init__.py
@@ -2,13 +2,14 @@
 Presencify - A Discord Rich Presence Manager for Windows
 This application is not affiliated with Discord in any way.
 """
-
 from .runtime import Runtime
 from .logger import Logger
 from .presence import Presence
+from .constants import Constants
+from .utils import Utils
 
 __title__ = "Presencify"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "Manuel Cabral"
 __license__ = "GNU GPLv3"
-__all__ = ["Runtime", "Logger", "Presence"]
+__all__ = ["Runtime", "Logger", "Presence", "Constants", "Utils"]
