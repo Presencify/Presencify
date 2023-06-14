7a56bff08c7ab93b80b1c63c0d69fd3bc9113d81
Manu <mcabral@frba.utn.edu.ar>
2023-06-14 19:14:14 -0300

    feat(runtime): add tab and remove ws from runtime

--- presencify/runtime.py
+++ presencify/runtime.py
@@ -1,6 +1,3 @@
-"""
-Note, this is a beta feature and may not work as expected.
-"""
 import json
 import httpx
 import subprocess as sp
@@ -24,149 +21,152 @@ class MediaSession:
         )
 
 
+class Tab:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __eq__(self, other):
+        if not isinstance(other, Tab):
+            return False
+        return self.url == other.url
+
+    def __repr__(self):
+        return f"<Tab url={self.url}>"
+
+    def __str__(self):
+        return self.url
+
+    def execute(self, code: str) -> None:
+        ws = create_connection(self.webSocketDebuggerUrl)
+        ws.send(
+            json.dumps(
+                {"id": 1, "method": "Runtime.evaluate", "params": {"expression": code}}
+            )
+        )
+        data = json.loads(ws.recv())
+        ws.close()
+        _type = data["result"]["result"]["type"]
+        if _type == "object" or _type == "undefined":
+            return None
+        return data["result"]["result"]["value"]
+
+    def media_session(self) -> MediaSession:
+        """
+        Get the current media session.
+        """
+        data = self.execute(
+            "[navigator.mediaSession.playbackState,\
+                        navigator.mediaSession.metadata.album || '',\
+                        navigator.mediaSession.metadata.artist,\
+                        navigator.mediaSession.metadata.artwork[0].src,\
+                        navigator.mediaSession.metadata.title].join('@')"
+        )
+        if data is None:
+            return None
+        data = data.split("@")
+        data = {
+            "state": data[0],
+            "album": data[1],
+            "artist": data[2],
+            "image": data[3],
+            "title": data[4],
+        }
+        return MediaSession(**data)
+
+
 class Runtime:
-    """
-    Used to execute some JavaScript code in the browser.
-    """
-
-    __slots__ = (
-        "__pages",
-        "__enable",
-        "__current_tab",
-        "__ws",
-        "__port",
-        "__connected",
-    )
-
-    def __init__(self, port: int):
+    def __init__(self, port: int, origin: str):
         self.__port = port
+        self.__tabs = []
+        self.__origin = origin
         self.__connected = False
-        self.__current_tab = self.__ws = None
+        self.__current_tab = None
 
     @property
     def connected(self) -> bool:
         return self.__connected
 
-    def __update(self):
-        if not self.__connected:
-            return
-        self.__pages = self.__request()
-        if self.__pages == []:
+    @property
+    def current_tab(self) -> Tab:
+        self.__update()
+        return self.__current_tab
+
+    def __req(self) -> list:
+        """
+        Request the remote browser for get the tabs.
+        """
+        try:
+            res = httpx.get(Constants.REMOTE_URL.format(port=self.__port))
+            return res.json()
+        except Exception as exc:
             Logger.write(
-                msg="Remote browser closed unexpectedly!",
+                msg=f"{self.__origin} failed to connect and get tabs: {exc}",
                 origin=self,
                 level="error",
             )
-            self.__connected = False
-            return
-        self.__pages = self.__filter(self.__pages)
-        self.__current_tab = self.__pages[0]
-        self.__ws_close()
-        self.__ws = create_connection(self.__current_tab["webSocketDebuggerUrl"])
-
-    def __request(self) -> dict:
-        try:
-            response = httpx.get(Constants.REMOTE_URL.format(port=self.__port))
-            return response.json()
-        except Exception as exc:
             return []
 
-    def __filter(self, data: dict) -> dict:
-        return [element for element in data if element["type"] == "page"]
-
-    def __ws_close(self):
-        if self.__ws and self.__ws.connected:
-            self.__ws.close()
-
-    def close(self) -> None:
+    def __update(self):
+        """
+        Update tabs and return the current tab or the tab specified.
+        """
         if not self.__connected:
             Logger.write(
-                msg="Cannot close a connection that is not open!",
+                msg=f"Remote browser update failed for {self.__origin}: not connected!",
+                origin=self,
+                level="warning",
+            )
+            return
+        try:
+            tabs = self.__req()
+            if len(tabs) == 0:
+                raise RuntimeError("No information received from the remote browser")
+            self.__tabs = [tab for tab in tabs if tab["type"] == "page"]
+            if len(self.__tabs) == 0:
+                raise RuntimeError("No tabs found in the remote browser")
+            self.__tabs = [Tab(**tab) for tab in self.__tabs]
+            self.__current_tab = self.__tabs[0]
+            Logger.write(msg=f"Updated tabs in {self.__origin}!", origin=self)
+        except Exception as exc:
+            self.__connected = False
+            self.__tabs = []
+            Logger.write(
+                msg=f"Failed to update tabs in {self.__origin}: {exc}",
                 origin=self,
                 level="error",
             )
-            return
-        self.__ws_close()
-        self.__connected = False
-        Logger.write(msg="Closed connection with browser", origin=self)
 
-    @property
-    def url(self) -> str:
-        self.__update()
-        return self.__current_tab["url"]
-
-    @property
-    def title(self) -> str:
-        return self.__current_tab["title"]
-
-    @property
-    def icon(self) -> str:
-        return self.__current_tab["faviconUrl"]
-
-    def mediaSession(self):
-        # TODO: make a better implementation
-        if not self.__connected:
-            return None
-        self.__update()
-        self.__ws.send(
-            json.dumps(
-                {
-                    "id": 0,
-                    "method": "Runtime.evaluate",
-                    "params": {
-                        "expression": "\
-                        [navigator.mediaSession.playbackState,\
-                        navigator.mediaSession.metadata.album,\
-                        navigator.mediaSession.metadata.artist,\
-                        navigator.mediaSession.metadata.artwork[0].src,\
-                        navigator.mediaSession.metadata.title].join('@')",
-                    },
-                }
-            )
-        )
-        data = json.loads(self.__ws.recv())
-        if data["result"]["result"]["type"] == "object":
-            return None
-        data = data["result"]["result"]["value"].split("@")
-        return MediaSession(
-            playbackState=data[0],
-            album=data[1],
-            artist=data[2],
-            image=data[3],
-            title=data[4],
-        )
-
-    def execute(self, code: str) -> None:
-        if not self.__connected:
-            return
-        self.__update()
-        self.__ws.send(
-            json.dumps(
-                {
-                    "id": 0,
-                    "method": "Runtime.evaluate",
-                    "params": {"expression": code},
-                }
-            )
-        )
-        data = json.loads(self.__ws.recv())
-        if data["result"]["result"]["type"] == "undefined":
-            return None
-        return data["result"]["result"]["value"]
+    def tabs(self, url_pattern: str = None, force_update: bool = False) -> list:
+        """
+        Get all tabs or all tabs with the specified url pattern.
+        """
+        if force_update:
+            self.__update()
+        if url_pattern is None:
+            return self.__tabs
+        return [tab for tab in self.__tabs if url_pattern in tab.url]
 
     def connect(self) -> None:
+        """
+        Connect to the remote browser.
+        """
         if self.__connected:
+            Logger.write(
+                msg="Remote browser already connected!", origin=self, level="warning"
+            )
             return
-        data = self.__request()
-        data = self.__filter(data)
-        if len(data) == 0:
-            raise RuntimeError("No pages found")
-        self.__current_tab = data[0]
+        self.__update()
         self.__connected = True
-        Logger.write(
-            msg=f"Browser remote instance connected successfully",
-            origin=self,
-        )
+        Logger.write(msg=f"Remote browser for {self.__origin} connected!", origin=self)
 
-    def is_enabled(self) -> bool:
-        return self.__connected
+    def close(self) -> None:
+        if self.__connected is False:
+            Logger.write(
+                msg=f"Remote browser for {self.__origin} already disconnected!",
+                origin=self,
+            )
+            return
+        self.__connected = False
+        Logger.write(
+            msg=f"Remote browser for {self.__origin} disconnected!", origin=self
+        )
